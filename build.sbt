41be26df1769a3987e580afb65a2e0b14f561881
Jacek Tokar <tokar.j@pg.com>
2019-12-24 10:23:05 +0100

    works from DBR notebook

---
+++ build.sbt
@@ -0,0 +1,28 @@
+name := "Promoter"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
+
+val sparkVersion = "2.4.4"
+
+//unmanagedBase := file("/Users/jacektokar/miniconda3/lib/python3.7/site-packages/pyspark/jars")
+
+libraryDependencies ++= Seq(
+
+  "org.apache.spark" % "spark-sql_2.11" % "2.4.4" % "provided",
+  "org.apache.spark" % "spark-core_2.11" % "2.4.4" % "provided",
+  "org.apache.spark" %% "spark-mllib" % "2.4.4" % "provided",
+
+
+  //  "org.slf4j" % "slf4j-api" % "1.7.5",
+  //  "org.slf4j" % "slf4j-simple" % "1.7.5",
+
+  // hadoop
+  "org.apache.hadoop" % "hadoop-common" % "2.7.3" % "provided" ,
+ // "org.apache.commons" % "commons-io" % "1.3.2" % "provided" ,
+  "org.apache.hadoop" % "hadoop-hdfs" % "2.7.3" % "provided",
+  "com.databricks" % "dbutils-api_2.11" % "0.0.4" % "provided"
+
+)
+
--- src/main/scala/com/pg/bigdata/utils/Promotor.scala
+++ src/main/scala/com/pg/bigdata/utils/Promotor.scala
@@ -1,14 +1,32 @@
-package com.pg.bigdata.utils
+case class Paths(sourcePath: String, targetPath: String)
 
+import com.pg.bigdata.utils.ConfigSerDeser
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.SparkSession
+import com.databricks.dbutils_v1.DBUtilsHolder.dbutils
 
-
-object Promotor {
-  case class Paths(sourcePath: String, targetPath: String)
+object Promotor extends Serializable {
   val magicPrefix = ".dfs.core.windows.net"
-  private def getTableLocation(databaseName: String, tableName: String)(implicit spark: SparkSession): String = {
+
+  def getListOfTableFiles(sourceDbName: String, sourceTableName: String)(implicit spark: SparkSession) = {
+    spark.table(sourceDbName + "." + sourceTableName).inputFiles.map(x => Promotor.getRelativePath(x))
+
+  }
+
+
+  def getPathsList(sourceDbName: String, sourceTableName: String,
+                   targetDbName: String, targetTableName: String)(implicit spark: SparkSession) = {
+    val sourceLocation = Promotor.getRelativePath(Promotor.getTableLocation(sourceDbName,sourceTableName))
+    val targetLocation = Promotor.getRelativePath(Promotor.getTableLocation(targetDbName,targetTableName))
+    println("target location " +targetLocation)
+    val sourceFileList = getListOfTableFiles(sourceDbName, sourceTableName)
+    val targetFileList = sourceFileList.map(_.replaceAll(sourceLocation, targetLocation))
+    println(targetFileList(0))
+    sourceFileList.zip(targetFileList).map(x => Paths(x._1,x._2))
+  }
+
+  def getTableLocation(databaseName: String, tableName: String)(implicit spark: SparkSession): String = {
     import org.apache.spark.sql.catalyst.TableIdentifier
     spark.catalog.setCurrentDatabase(databaseName)
     val tblMetadata = spark.sessionState.catalog.getTableMetadata(new TableIdentifier(tableName))
@@ -19,21 +37,25 @@ object Promotor {
     getTableLocation(spark.catalog.currentDatabase,tableName)
   }
 
-   def getListOfTableFiles(sourceDbName: String, sourceTableName: String)(implicit spark: SparkSession) = {
-    spark.table(sourceDbName + "." + sourceTableName).inputFiles.map(x => getRelativePath(x))
+  def copySingleFile(hadoopConf: Configuration, sourcePath: String, targetPath: String,
+                     sourceFileSystem: FileSystem, targetFileSystem: FileSystem,
+                     overwrite: Boolean = true, deleteSource: Boolean = false) = {
+    println(sourcePath + " => " + targetPath)
+    val srcPath = new Path(sourcePath)
+    val destPath = new Path(targetPath)
+    org.apache.hadoop.fs.FileUtil.copy(sourceFileSystem, srcPath, targetFileSystem, destPath, deleteSource, overwrite, hadoopConf)
+  }
 
+  def getFileSystemPrefix(uri: String): String = {
+    // val magicPrefix = ".dfs.core.windows.net"
+    uri.substring(0,uri.indexOf(magicPrefix)+magicPrefix.length)
   }
-  //gets a list of source and target relative paths (relative to base file system)
-  def getPathsList(sourceDbName: String, sourceTableName: String,
-                             targetDbName: String, targetTableName: String)(implicit spark: SparkSession) = {
-    val sourceLocation = getTableLocation(sourceDbName,sourceTableName)
-    val targetLocation = getTableLocation(targetDbName,targetTableName)
-    val sourceFileList = getListOfTableFiles(sourceDbName, sourceTableName).take(5)  //todo remove take
-    val targetFileList = sourceFileList.map(_.replaceAll(sourceLocation, targetLocation))
-    sourceFileList.zip(targetFileList).map(x => Paths(x._1,x._2))
-  }
+
+
+
 
   def getRelativePath(uri: String): String = {
+    // val magicPrefix = ".dfs.core.windows.net"
     uri.substring(uri.indexOf(magicPrefix)+magicPrefix.length)
   }
 
@@ -41,38 +63,41 @@ object Promotor {
     uri.substring(uri.indexOf("//")+2,uri.indexOf("@"))
   }
 
-  def getFileSystemPrefix(uri: String): String = {
-    uri.substring(0,uri.indexOf(magicPrefix)+magicPrefix.length)
-  }
-
   def getFileSystem(hadoopConf: Configuration, absoluteTargetLocation: String) = {
-    hadoopConf.set("fs.defaultFS", getFileSystemPrefix(absoluteTargetLocation))
+    hadoopConf.set("fs.defaultFS", Promotor.getFileSystemPrefix(absoluteTargetLocation))
     FileSystem.get(hadoopConf)
   }
 
-  def copySingleFile(hadoopConf: Configuration, sourcePath: String, targetPath: String,
-                     sourceFileSystem: FileSystem, targetFileSystem: FileSystem,
-                     overwrite: Boolean = true, deleteSource: Boolean = false) = {
-    val srcPath = new Path(sourcePath)
-    val destPath = new Path(targetPath)
-    org.apache.hadoop.fs.FileUtil.copy(sourceFileSystem, srcPath, targetFileSystem, destPath, deleteSource, overwrite, hadoopConf)
-  }
-
   def copyFilesBetweenTables(sourceDbName: String, sourceTableName: String,
-    targetDbName: String, targetTableName: String, partitionCount: Int = 192)(implicit spark: SparkSession): Boolean = {
-    import spark.implicits._
-    val paths = getPathsList(sourceDbName, sourceTableName,targetDbName, targetTableName)
+                             targetDbName: String, targetTableName: String, partitionCount: Int = 192)(implicit spark: SparkSession): Boolean = {
+    //import spark.implicits._
+    val paths = Promotor.getPathsList(sourceDbName, sourceTableName,targetDbName, targetTableName).take(5)
     if(paths.isEmpty)
       throw new Exception("No files to be copied")
-    val srcLoc = getTableLocation(sourceDbName,sourceTableName)
-    val trgLoc = getTableLocation(targetDbName,targetTableName)
-    val sdConf = new ConfigSerDeser(spark.sparkContext.hadoopConfiguration)
+    val srcLoc = Promotor.getTableLocation(sourceDbName,sourceTableName)
+    val trgLoc = Promotor.getTableLocation(targetDbName,targetTableName)
+    val storageName = "***REMOVED***"
+    val principalID = "b74d7952-eea1-43bc-b0bb-a1088e6d32f5"
+    val secretName = "propensity-storage-principal"
+    val secretScopeName = "propensity"
+    val containerName = "dev"
+    val conf = spark.sparkContext.hadoopConfiguration
+    conf.set("fs.azure.account.auth.type." + storageName + ".dfs.core.windows.net", "OAuth")
+    //conf.set("fs.azure.account.key.***REMOVED***.dfs.core.windows.net","***REMOVED***") 
+    conf.set("fs.azure.account.oauth.provider.type." + storageName + ".dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+    conf.set("fs.azure.account.oauth2.client.id." + storageName + ".dfs.core.windows.net", principalID)
+    conf.set("fs.azure.account.oauth2.client.secret." + storageName + ".dfs.core.windows.net", dbutils.secrets.get(scope = secretScopeName, key = secretName))
+    conf.set("fs.azure.account.oauth2.client.endpoint." + storageName + ".dfs.core.windows.net", "https://login.microsoftonline.com/3596192b-fdf5-4e2c-a6fa-acb706c963d8/oauth2/token")
+
+    val sdConf = new ConfigSerDeser(conf)
+    print(paths(0))
     val res = spark.sparkContext.parallelize(paths).mapPartitions(x => {
       val conf = sdConf.get
-      val srcFs = getFileSystem(conf, srcLoc)
-      val trgFs = getFileSystem(conf, trgLoc)
+      //conf.set("fs.azure.account.key.***REMOVED***.dfs.core.windows.net", "***REMOVED***")
+      val srcFs = Promotor.getFileSystem(conf, srcLoc)
+      val trgFs = Promotor.getFileSystem(conf, trgLoc)
       x.map(paths => {
-        (paths, copySingleFile(conf, paths.sourcePath,paths.targetPath,srcFs,trgFs))
+        (paths, Promotor.copySingleFile(conf, paths.sourcePath,paths.targetPath,srcFs,trgFs))
       })
     })
     res.cache()

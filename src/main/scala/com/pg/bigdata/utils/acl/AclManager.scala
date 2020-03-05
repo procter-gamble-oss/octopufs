992ba3a61d099697cdf2fae638a26ccebdb6793c
Jacek Tokar <tokar.j@pg.com>
2020-03-05 17:43:06 +0100

    added get size in bytes

--- src/main/scala/com/pg/bigdata/utils/acl/AclManager.scala
+++ src/main/scala/com/pg/bigdata/utils/acl/AclManager.scala
@@ -2,25 +2,24 @@ package com.pg.bigdata.utils.acl
 
 import java.util.concurrent.Executors
 
-import com.pg.bigdata.utils.fs._
-import com.pg.bigdata.utils.metastore._
-import com.pg.bigdata.utils.fs.FSOperationResult
+import com.pg.bigdata.utils.fs.{FSOperationResult, _}
 import com.pg.bigdata.utils.helpers.ConfigSerDeser
+import com.pg.bigdata.utils.metastore._
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.fs.permission.{AclEntry, AclEntryScope, AclEntryType, FsAction}
 import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
+import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
-import collection.JavaConverters._
 import scala.util.Try
 
 object AclManager extends Serializable {
 
   def modifyTableACLs(db: String, tableName: String, newPermission: FSPermission, partitionCount: Int = 30)
-                     (implicit spark: SparkSession, confEx: Configuration): Dataset[FSOperationResult] = {
+                     (implicit spark: SparkSession, confEx: Configuration): Array[FSOperationResult] = {
     import collection.JavaConverters._
 
     val sdConf = new ConfigSerDeser(confEx)
@@ -29,29 +28,50 @@ object AclManager extends Serializable {
 
     getFileSystem(confEx, loc).modifyAclEntries(new Path(getRelativePath(loc)), Seq(AclManager.getAclEntry(newPermission.getDefaultLevelPerm())).asJava)
 
-    println(files(0))
+    println(files.head)
     println("Files to process: " + files.length)
-    modifyACL(files,loc, partitionCount,sdConf,newPermission)
+    modifyACL(files, loc, partitionCount, sdConf, newPermission)
 
   }
 
+  def modifyFolderACLs(folderUri: String, newPermission: FSPermission, partitionCount: Int = 30, driverParallelism: Int = 1000)(implicit spark: SparkSession, confEx: Configuration): Array[FSOperationResult] = {
+    implicit val pool = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(driverParallelism))
+    val sdConf = new ConfigSerDeser(confEx)
+    val fs = getFileSystem(confEx, folderUri)
+    val elements = listRecursively(fs, new Path(folderUri))
+    val folders = elements.filter(_.isDirectory).map(_.path)
+    val files = elements.filter(!_.isDirectory).map(_.path)
+
+    println(files(0))
+    println("Files to process: " + files.length)
+    println("Folders to process: " + folders.length)
+
+    println("Changing file and folders ACCESS ACLs: " + files.length)
+    val resAccess = modifyACL(elements.map(_.path).toList, folderUri, partitionCount, sdConf, newPermission)
+    println("Changing folders Default ACLs: " + folders.length)
+    val resDefault = modifyACL(folders.toList, folderUri, partitionCount, sdConf, newPermission.getDefaultLevelPerm())
+    resAccess.union(resDefault)
+  }
+
   def modifyACL(paths: List[String], parentFolderURI: String, partitionCount: Int, sdConf: ConfigSerDeser, newFsPermission: FSPermission)
-               (implicit spark: SparkSession): Dataset[FSOperationResult] = {
+               (implicit spark: SparkSession): Array[FSOperationResult] = {
     import scala.concurrent.ExecutionContext.Implicits.global
-    val res = spark.sparkContext.parallelize(paths, partitionCount).mapPartitions(part => {
+    spark.sparkContext.parallelize(paths, partitionCount).mapPartitions(part => {
       val conf = sdConf.get()
       val y = AclManager.getAclEntry(newFsPermission)
       val fs = getFileSystem(conf, parentFolderURI)
+      val parentFolder = new Path(getRelativePath(parentFolderURI))
+      if (fs.isDirectory(parentFolder))
+        fs.modifyAclEntries(parentFolder, Seq(AclManager.getAclEntry(newFsPermission.getDefaultLevelPerm())).asJava)
+
       part.map(x => Future {
         Try({
           fs.modifyAclEntries(new Path(x), Seq(y).asJava)
-          (x, true)
-        }).getOrElse((x, false)) //todo make sure it executes only once
+          FSOperationResult(x, true)
+        }).getOrElse(FSOperationResult(x, false))
       }).map(x => Await.result(x, 1.minute))
     }
-    )
-    import spark.implicits._
-    spark.createDataset(res).toDF("path", "success").as[FSOperationResult]
+    ).collect()
   }
 
   def getAclEntry(p: FSPermission): AclEntry = {
@@ -72,26 +92,6 @@ object AclManager extends Serializable {
       setName(p.granteeObjectId).build()
     println(y.toString)
     y
-  }
-
-  def modifyFolderACLs(folderUri: String, newPermission: FSPermission, partitionCount: Int = 30, driverParallelism: Int = 1000)(implicit spark: SparkSession, confEx: Configuration): DataFrame = {
-    implicit val pool = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(driverParallelism))
-    val sdConf = new ConfigSerDeser(confEx)
-    val fs = getFileSystem(confEx, folderUri)
-    val elements = listRecursively(fs, new Path(folderUri))
-    val folders = elements.filter(_.isDirectory).map(_.path)
-    val files = elements.filter(!_.isDirectory).map(_.path)
-
-    println(files(0))
-    println("Files to process: " + files.length)
-    println("Folders to process: " + folders.length)
-
-    println("Changing file and folders ACCESS ACLs: " + files.length)
-    val resAccess = modifyACL(elements.map(_.path).toList, folderUri,partitionCount,sdConf,newPermission)
-    println("Changing folders Default ACLs: " + folders.length)
-    val resDefault = modifyACL(folders.toList,folderUri,partitionCount,sdConf,newPermission.getDefaultLevelPerm())
-    resAccess.withColumn("acl_type", lit("ACCESS")).
-      union(resDefault.withColumn("acl_type", lit("DEFAULT")))
   }
 
   case class FSPermission(scope: String, permission: String, level: String, granteeObjectId: String) {
--- src/main/scala/com/pg/bigdata/utils/fs/package.scala
+++ src/main/scala/com/pg/bigdata/utils/fs/package.scala
@@ -1,46 +1,59 @@
 package com.pg.bigdata.utils
 
+import java.util.concurrent.Executors
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 
-import scala.concurrent.{Await, ExecutionContextExecutor, Future}
 import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
 
 package object fs {
   var magicPrefix = ".dfs.core.windows.net"
 
   def getContainerName(uri: String): String = {
-    if(uri.contains("@"))
-      uri.substring(uri.indexOf("//")+2,uri.indexOf("@"))
+    if (uri.contains("@"))
+      uri.substring(uri.indexOf("//") + 2, uri.indexOf("@"))
     else
       "noContainerInPath"
   }
 
   def getRelativePath(uri: String): String = {
-    if(magicPrefix=="") uri
-    else if(!uri.contains(magicPrefix))
+    if (magicPrefix == "") uri
+    else if (!uri.contains(magicPrefix))
       throw new Exception("MagicPrefix not found")
     else
-      uri.substring(uri.indexOf(magicPrefix)+magicPrefix.length)
+      uri.substring(uri.indexOf(magicPrefix) + magicPrefix.length)
   }
 
   def getFileSystem(hadoopConf: Configuration, absoluteTargetLocation: String): FileSystem = {
-    if(!getFileSystemPrefix(absoluteTargetLocation).startsWith("file")) //this is to avoid failures on local fs
+    if (!getFileSystemPrefix(absoluteTargetLocation).startsWith("file")) //this is to avoid failures on local fs
       hadoopConf.set("fs.defaultFS", getFileSystemPrefix(absoluteTargetLocation))
     FileSystem.get(hadoopConf)
   }
 
   def getFileSystemPrefix(uri: String): String = {
-    if(!uri.contains(magicPrefix))
-      throw new Exception("MagicPrefix "+magicPrefix+" not found in "+uri)
-    uri.substring(0,uri.indexOf(magicPrefix)+magicPrefix.length)
+    if (!uri.contains(magicPrefix))
+      throw new Exception("MagicPrefix " + magicPrefix + " not found in " + uri)
+    uri.substring(0, uri.indexOf(magicPrefix) + magicPrefix.length)
   }
 
-  def listRecursively(fs: FileSystem, sourceFolder: Path, timeoutMin: Int=20)(implicit pool: ExecutionContextExecutor): Array[FSElement] = {
+  def listRecursively(fs: FileSystem, sourceFolder: Path, timeoutMin: Int = 20)(implicit pool: ExecutionContextExecutor): Array[FSElement] = {
     val elements = fs.listStatus(sourceFolder)
     val folders = elements.filter(_.isDirectory)
-    if(folders.isEmpty) elements.filter(!_.isDirectory).map(x => FSElement(x.getPath.toString, false))
-    else folders.map(folder => Future{listRecursively(fs, folder.getPath)}(pool)).flatMap(x => Await.result(x, timeoutMin.minutes)) ++
-      folders.map(x => FSElement(x.getPath.toString, true))
+    if (folders.isEmpty) elements.filter(!_.isDirectory).map(x => FSElement(x.getPath.toString, false, x.getLen))
+    else folders.map(folder => Future {
+      listRecursively(fs, folder.getPath)
+    }(pool)).flatMap(x => Await.result(x, timeoutMin.minutes)) ++
+      folders.map(x => FSElement(x.getPath.toString, true, x.getLen))
+  }
+
+  def getSizeInMB(path: String, driverParallelism: Int = 100, timeoutInMin: Int = 20)(implicit conf: Configuration): Double = {
+    val fs = FileSystem.get(conf)
+    val pool = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(driverParallelism))
+    val files = listRecursively(fs, new Path(path), timeoutInMin)(pool)
+    val sizeInMb = files.map(_.byteSize).sum.toDouble / 1024 / 1024
+    println("Size of " + path + " is " + (sizeInMb*1000).round.toDouble/1000 + " MB")
+    sizeInMb
   }
 }

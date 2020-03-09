b40b44f398402d77a7a2d962f8d19ae98e610dd7
Jacek Tokar <tokar.j@pg.com>
2020-03-09 11:47:02 +0100

    added local move and delete

--- src/test/scala/AclTest.scala
+++ src/test/scala/AclTest.scala
@@ -1,5 +1,9 @@
-import org.apache.spark.sql.SparkSession
+import java.util.concurrent.Executors
 
+import org.apache.spark.sql.SparkSession
+import com.pg.bigdata.utils.fs._
+
+import scala.concurrent.ExecutionContext
 object AclTest extends App{
   import com.pg.bigdata.utils.acl.AclManager.FsPermission
   import com.pg.bigdata.utils._
@@ -12,6 +16,10 @@ object AclTest extends App{
   val acl = AclManager.FsPermission("user", "r-x","ACCESS","11f1d713-3b19-49ec-bade-cca9a2e2a3ba")
   //implicit val s = spark
   implicit val c = spark.sparkContext.hadoopConfiguration
-  c.set("fs.azure.account.key.adls2nas001.dfs.core.windows.net","YOURKEY")
-  AclManager.modifyTableACLs("dp_neighborhood_sales","store_sales_fct_jt",acl, 30)
+  //c.set("fs.azure.account.key.adls2nas001.dfs.core.windows.net","YOURKEY")
+  magicPrefix="hdfs://localhost:8020"
+  val fs = getFileSystem(c,"hdfs://localhost:8020/test1")
+  implicit val pool = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(100))
+  synchronizeAcls(fs,"/test1","/test2")
+  //AclManager.modifyTableACLs("dp_neighborhood_sales","store_sales_fct_jt",acl, 30)
 }

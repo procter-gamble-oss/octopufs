3d416dabf411d9eef305f91cbacc614c33d761e3
Jacek Tokar <tokar.j@pg.com>
2020-06-18 22:59:13 +0200

    scaladoc 5

--- src/main/scala/com/pg/bigdata/octopufs/fs/LocalExecution.scala
+++ src/main/scala/com/pg/bigdata/octopufs/fs/LocalExecution.scala
@@ -1,6 +1,5 @@
 package com.pg.bigdata.octopufs.fs
 
-import java.util.concurrent.{ExecutorService, Executors}
 
 import com.pg.bigdata.octopufs.SafetyFuse
 import org.apache.hadoop.conf.Configuration
@@ -11,9 +10,20 @@ import scala.concurrent.{Await, Future}
 
 import com.pg.bigdata.octopufs.helpers.implicits._
 
+/**
+ * Object contains functions which can be locally multithreaded (e.g. copy and move operations)
+ */
 object LocalExecution extends Serializable {
 
-  def moveFolderContent(sourceFolderUri: String, targetFolderUri: String, keepSourceFolder: Boolean = false, timeoutMin: Int = 10)
+  /**
+   * Moves all files from source folder and moves them to target folder. Operations are executed in parallel on local node. Operation is rerun safe.
+   * @param sourceFolderUri - source folder full path, i.e. abfss://cont@cont.dfs....
+   * @param targetFolderUri - target folder full path, i.e. abfss://cont@cont.dfs....
+   * @param keepSourceFolder - controls if source folder should be deleted or not
+   * @param conf - Hadoop Configuration [[org.apache.hadoop.conf.Configuration]]
+   * @return - information about all processed paths together with information of operation was successful or not
+   */
+  def moveFolderContent(sourceFolderUri: String, targetFolderUri: String, keepSourceFolder: Boolean = false)
                        (implicit conf: Configuration): Array[FsOperationResult] = {
     println("Moving folders content: " + sourceFolderUri + "  ==>>  " + targetFolderUri)
     implicit val srcFs: FileSystem = getFileSystem(conf,sourceFolderUri)
@@ -23,16 +33,15 @@ object LocalExecution extends Serializable {
     if (!doesMoveLookSafe(srcFs,sourceFolderUri, targetFolderUri))
       return Array[FsOperationResult]()
 
-    //delete target folder
+    //delete target foldet
     val transaction = new SafetyFuse("data/movefolderContent")
 
     if (!transaction.isInProgress()) {
-      transaction.startTransaction()
-
       if (srcFs.exists(new Path(targetFolderUri)))
         deleteFolder(targetFolderUri, true)
       else
         srcFs.mkdirs(new Path(targetFolderUri))
+      transaction.startTransaction()
     }
 
     val sourceFileList = srcFs.listStatus(new Path(sourceFolderUri)).map(x => x.getPath.toString)
@@ -51,8 +60,14 @@ object LocalExecution extends Serializable {
     res
   }
 
-  def movePaths(paths: Array[Paths], timeoutMins: Int = 10, attempt: Int = 0)
-               (implicit conf: Configuration): Array[FsOperationResult] = {
+  /**
+   * Executes move operation on provided collection of [[com.pg.bigdata.octopufs.fs.Paths]]
+   * @param paths - collection of files ([[com.pg.bigdata.octopufs.fs.Paths]]) to move
+   * @param attempt - no not use
+   * @param conf - Hadoop Configuration object [[org.apache.hadoop.conf.Configuration]]
+   * @return - information about all processed paths together with information of operation was successful or not
+   */
+  def movePaths(paths: Array[Paths], attempt: Int = 0)(implicit conf: Configuration): Array[FsOperationResult] = {
     println("Starting moveFiles - attempt: " + attempt + ". Paths to be moved: " + paths.length)
     implicit val srcFs: FileSystem = getFileSystem(conf,paths.head.sourcePath)
     val trgFs = getFileSystem(conf,paths.head.targetPath) //needed just to check if both fs are the same
@@ -61,7 +76,7 @@ object LocalExecution extends Serializable {
     val res = paths.map(x => Future {
       print(".")
       FsOperationResult(x.sourcePath, srcFs.rename(new Path(x.sourcePath), new Path(x.targetPath)))
-    }).map(x => Await.result(x, timeoutMins.minutes))
+    }).map(x => Await.result(x, fsOperationTimeoutMinutes.minutes))
 
     println("Number of files moved properly: " + res.count(_.success))
     println("Files with errors: " + res.count(!_.success))
@@ -76,18 +91,25 @@ object LocalExecution extends Serializable {
         reallyFailed.map(_.sourcePath).slice(0, 10).mkString("\n"))
     else {
       val pathsForReprocessing = reallyFailed
-      res.filter(_.success) ++ movePaths(pathsForReprocessing, timeoutMins, attempt + 1)(conf)
+      res.filter(_.success) ++ movePaths(pathsForReprocessing, attempt + 1)(conf)
     }
   }
 
-  def deletePaths(paths: Array[String], timeoutMin: Int = 10, attempt: Int = 0)(implicit confEx: Configuration): Array[FsOperationResult] = {
+  /**
+   * Deletes all files/folders provided in collection of absolute paths
+   * @param paths - collection of paths, e.g. abfss://cont@storage.dfs....
+   * @param attempt - do not use
+   * @param conf - Hadoop Configuration object [[org.apache.hadoop.conf.Configuration]]
+   * @return - information about all processed paths together with information of operation was successful or not
+   */
+  def deletePaths(paths: Array[String], attempt: Int = 0)(implicit conf: Configuration): Array[FsOperationResult] = {
     //todo add check if all paths are from the same fs
-    val fs = getFileSystem(confEx, paths(0))
+    val fs = getFileSystem(conf, paths(0))
     val res = paths.map(x =>
       Future {
         FsOperationResult(x, fs.delete(new Path(x), true))
       }
-    ).map(x => Await.result(x, timeoutMin.minutes))
+    ).map(x => Await.result(x, fsOperationTimeoutMinutes.minutes))
 
     val failed = res.filter(!_.success)
     println("Number of paths deleted properly: " + res.count(_.success == true))
@@ -96,11 +118,17 @@ object LocalExecution extends Serializable {
     else if (failed.length == paths.length || attempt > 4)
       throw new Exception("Delete of some paths did not succeed - please check why and here are some of them: \n" + failed.map(_.path).slice(0, 10).mkString("\n"))
     else
-      res.filter(_.success) ++ deletePaths(paths.filter(x => failed.map(_.path).contains(x)), timeoutMin, attempt + 1)
+      res.filter(_.success) ++ deletePaths(paths.filter(x => failed.map(_.path).contains(x)), attempt + 1)
   }
 
-  def deleteFolder(folderUriPath: String, deleteContentOnly: Boolean = false, timeoutMin: Int = 10)(implicit confEx: Configuration): Unit = {
-    val fs = getFileSystem(confEx, folderUriPath)
+  /**
+   * Deletes a folder or only content of the folder, depending on deleteContentOnly parameter
+   * @param folderUriPath - absoluite path of the folder, e.g abfss://cont@storage.dfs.......
+   * @param deleteContentOnly - if true, folder will not be deleted, only the content
+   * @param conf - Hadoop Configuration object [[org.apache.hadoop.conf.Configuration]]
+   */
+  def deleteFolder(folderUriPath: String, deleteContentOnly: Boolean = false)(implicit conf: Configuration): Unit = {
+    val fs = getFileSystem(conf, folderUriPath)
     if (deleteContentOnly) {
       val objectList = fs.listStatus(new Path(folderUriPath))
       val paths = objectList.map(folderUriPath + "/" + _.getPath.getName)
@@ -109,16 +137,16 @@ object LocalExecution extends Serializable {
       println("Deleting paths count:" + paths.length)
       println("First 20 files for deletion:")
       paths.slice(0, 20).foreach(println)
-      deletePaths(paths, timeoutMin)
+      deletePaths(paths)
     }
     else if (!fs.delete(new Path(folderUriPath), true)) throw new Exception("Failed to remove " + folderUriPath)
   }
 
-  def getFalseNegatives(paths: Array[Paths])(implicit fs: FileSystem): Array[Paths] = {
+  private def getFalseNegatives(paths: Array[Paths])(implicit fs: FileSystem): Array[Paths] = {
     paths.filter(x => !fs.exists(new Path(x.sourcePath)) && fs.exists(new Path(x.targetPath)))
   }
 
-  def removeFalseNegatives(paths: Array[Paths])(implicit fs: FileSystem): Array[Paths] = {
+  private def removeFalseNegatives(paths: Array[Paths])(implicit fs: FileSystem): Array[Paths] = {
     paths.diff(getFalseNegatives(paths))
   }
 
--- src/main/scala/com/pg/bigdata/octopufs/metastore/package.scala
+++ src/main/scala/com/pg/bigdata/octopufs/metastore/package.scala
@@ -1,41 +1,84 @@
 package com.pg.bigdata.octopufs
 
 import com.pg.bigdata.octopufs.fs._
-import com.pg.bigdata.octopufs.metastore._
 import org.apache.hadoop.fs.Path
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.catalog.CatalogTable
 
 package object metastore {
-
+  /**
+   * Gets location of hive table in current database
+   *
+   * @param tableName
+   * @param spark
+   * @return String containing path to the table folder
+   */
   def getTableLocation(tableName: String)(implicit spark: SparkSession): String = {
-    getTableLocation(spark.catalog.currentDatabase,tableName)
+    getTableLocation(spark.catalog.currentDatabase, tableName)
   }
+
+  /**
+   * Returns a list of paths to partitions which name corresponds (contains) string(s) included in partitionsToKeepLike collection
+   *
+   * @param db                   - hive database name
+   * @param tableName            - hive table name
+   * @param partitionsToKeepLike - collection of partition names part for filtering
+   * @param spark
+   */
   def filterPartitions(db: String, tableName: String, partitionsToKeepLike: Seq[String])(implicit spark: SparkSession): Array[String] = {
-    getTableL1PartitionsPaths(db,tableName).filter(x => partitionsToKeepLike.exists(y => x.contains(y)))
+    getTableL1PartitionsPaths(db, tableName).filter(x => partitionsToKeepLike.exists(y => x.contains(y)))
   }
 
   //returns absolute paths of partitions
+  /**
+   * Returns paths to first level partitions of a table
+   *
+   * @param db        - hive database name
+   * @param tableName - hive table name
+   * @param spark
+   */
   def getTableL1PartitionsPaths(db: String, tableName: String)(implicit spark: SparkSession): Array[String] = {
-    val m = getTableMetadata(db,tableName).partitionColumnNames
-    if(m.isEmpty) throw new Exception("Table " + db + "." + tableName + " is not partitioned")
+    val m = getTableMetadata(db, tableName).partitionColumnNames
+    if (m.isEmpty) throw new Exception("Table " + db + "." + tableName + " is not partitioned")
     val absTblLoc = getTableLocation(db, tableName)
     val fs = getFileSystem(spark.sparkContext.hadoopConfiguration, absTblLoc)
     val partList = fs.listStatus(new Path(absTblLoc))
     partList.filter(_.isDirectory).map(absTblLoc + "/" + _.getPath.getName)
   }
 
+  /**
+   * Returns location of a folder assigned to the hive table
+   *
+   * @param databaseName - hive database name
+   * @param tableName    - hive table name
+   * @param spark
+   */
   def getTableLocation(databaseName: String, tableName: String)(implicit spark: SparkSession): String = {
     val loc = getTableMetadata(databaseName, tableName).location.toString
-    println(databaseName+"."+tableName+" location is "+loc)
+    println(databaseName + "." + tableName + " location is " + loc)
     loc
   }
 
+  /**
+   * Gets metadata of hive table
+   *
+   * @param databaseName
+   * @param tableName
+   * @param spark
+   * @return
+   */
   private def getTableMetadata(databaseName: String, tableName: String)(implicit spark: SparkSession): CatalogTable = {
     spark.sessionState.catalog.getTableMetadata(new TableIdentifier(tableName, Option(databaseName)))
   }
 
+  /**
+   * Refreshes hive metadata for hive table. It will also refresh partitions, if table is partitioned
+   *
+   * @param db    - hive database
+   * @param table - hive table name
+   * @param spark
+   */
   def refreshMetadata(db: String, table: String)(implicit spark: SparkSession): Unit = {
     val target = db + "." + table
     println("Refreshing metadata for " + target)
@@ -45,9 +88,16 @@ package object metastore {
       spark.catalog.recoverPartitions(target)
     }
   }
-   def getListOfTableFiles(sourceDbName: String, sourceTableName: String)(implicit spark: SparkSession): Array[String] = {
-      spark.table(sourceDbName + "." + sourceTableName).inputFiles
 
-    }
+  /**
+   * Gets array of paths to files of a table. The list of files comes from hive metastore cache.
+   * @param sourceDbName
+   * @param sourceTableName
+   * @param spark
+   */
+  def getListOfTableFiles(sourceDbName: String, sourceTableName: String)(implicit spark: SparkSession): Array[String] = {
+    spark.table(sourceDbName + "." + sourceTableName).inputFiles
+
+  }
 
 }
